c1f63012e23ef1a48a80270b462139636bfd4738
aidera <hope.aidera@gmail.com>
2020-09-13 15:13:14 +0300

    Added edit page

--- src/app/admin/pages/edit-page/edit-page.component.ts
+++ src/app/admin/pages/edit-page/edit-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {PostsService} from '../../../shared/services/posts.service';
+import {switchMap} from 'rxjs/operators';
+import {Post} from '../../../shared/models/Post';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-page',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPageComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  post: Post;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public postsService: PostsService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.route.params
+      .pipe(switchMap((params: Params) => {
+        return this.postsService.getById(params.id);
+      })
+      ).subscribe((post: Post) => {
+        this.post = post;
+        this.form = new FormGroup({
+          postTitle: new FormControl(post.title, Validators.required),
+          postText: new FormControl(post.text, Validators.required)
+        });
+    });
+
+  }
+
+  submit(): void {
+    if (!this.form.invalid) {
+      this.postsService.update({
+        ...this.post,
+        text: this.form.value.postText,
+        title: this.form.value.postTitle,
+      }).subscribe(() => {
+        this.form.reset();
+        this.router.navigate(['/admin', 'dashboard']);
+      });
+    }
+  }
 }
--- src/app/shared/services/posts.service.ts
+++ src/app/shared/services/posts.service.ts
@@ -1,38 +1,89 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {CreatePostResponse, Post} from '../models/Post';
 import {environment} from '../../../environments/environment';
-import { map } from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor(private http: HttpClient) { }
+  fetching = false;
+
+  constructor(private http: HttpClient) {
+  }
 
   create(post: Post): Observable<Post> {
+    this.fetching = true;
     return this.http.post(`${environment.dbUrl}/posts.json`, post)
-      .pipe(map((response: CreatePostResponse) => {
-        return {
-          ...post,
-          id: response.name,
-          date: new Date(post.date),
-        } as Post;
-      }));
+      .pipe(
+        map((response: CreatePostResponse) => {
+          return {
+            ...post,
+            id: response.name,
+            date: new Date(post.date),
+          } as Post;
+        }),
+        tap(() => {
+          this.fetching = false;
+        })
+      );
   }
 
   getAll(): Observable<Post[]> {
+    this.fetching = true;
     return this.http.get(`${environment.dbUrl}/posts.json`)
-      .pipe(map((response: {[key: string]: any}) => {
-        return Object
-          .keys(response)
-          .map(key => ({
-            ...response[key],
-            id: key,
-            date: new Date(response[key].date)
-          }));
+      .pipe(
+        map((response: { [key: string]: any }) => {
+          return Object
+            .keys(response)
+            .map(key => ({
+                ...response[key],
+                id: key,
+                date: new Date(response[key].date)
+              })
+            );
+        }),
+        tap(() => {
+          this.fetching = false;
+        }));
+  }
+
+  getById(id: string): Observable<Post> {
+    this.fetching = true;
+    return this.http.get<Post>(`${environment.dbUrl}/posts/${id}.json`)
+      .pipe(
+        map((post: Post) => {
+          return {
+            ...post,
+            id,
+            date: new Date(post.date),
+          } as Post;
+        }),
+        tap(() => {
+          this.fetching = false;
+        })
+      );
+  }
+
+  remove(id: string): Observable<void> {
+    this.fetching = true;
+    return this.http.delete<void>(`${environment.dbUrl}/posts/${id}.json`)
+      .pipe(tap(() => {
+        this.fetching = false;
+      }));
+  }
+
+  update(post: Post): Observable<Post> {
+    console.log('update start');
+    console.log(post);
+    this.fetching = true;
+    return this.http.patch<Post>(`${environment.dbUrl}/posts/${post.id}.json`, post)
+      .pipe(tap(() => {
+        console.log('update tap');
+        this.fetching = false;
       }));
   }
 }
